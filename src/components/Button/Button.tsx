2b625d3e493f43dc21bcaee5a4127f09797cb5fb
Dmitry Dadychyn <ddadychyn@elinext.com>
2024-11-01 19:23:37 +0300

    feat(f-4): add sidebar switch button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,49 @@
+import { ButtonHTMLAttributes, memo, ReactNode } from 'react';
+import classNames from 'classnames';
+
+import styles from './button.module.scss';
+
+export enum ThemeButton {
+    CLEAR = 'clear',
+    OUTLINE = 'outline',
+    OUTLINE_RED = 'outline_red',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string;
+    theme?: ThemeButton;
+    ref?: React.LegacyRef<HTMLButtonElement>;
+    disabled?: boolean;
+    children?: ReactNode;
+    fullWidth?: boolean;
+}
+
+export const Button = memo((props: ButtonProps) => {
+    const {
+        className,
+        children,
+        theme = ThemeButton.OUTLINE,
+        ref,
+        disabled,
+        fullWidth,
+        ...otherProps
+    } = props;
+
+    const mods = {
+        [styles.button]: !disabled,
+        [styles.disabledButton]: disabled,
+        [styles.fullWidth]: fullWidth,
+    };
+
+    return (
+        <button
+            type='button'
+            className={classNames({ ...mods }, className, styles[theme])}
+            ref={ref}
+            disabled={disabled}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+});
---
+++ src/const/localStorage.ts
@@ -0,0 +1,2 @@
+export const USER_LOCAL_STORAGE_KEY = 'user';
+export const LOCAL_STORAGE_SIDEBAR_SWITCHER_KEY = 'burger';
