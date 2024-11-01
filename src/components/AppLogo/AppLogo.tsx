b1e62f9e456dd25c1331cd373d9e4d870cebef7a
Dmitry Dadychyn <ddadychyn@elinext.com>
2024-11-01 12:58:19 +0300

    feat(f-2): add Navbar

---
+++ src/components/AppLogo/AppLogo.tsx
@@ -0,0 +1,11 @@
+import classNames from 'classnames';
+import styles from './AppLogo.module.scss';
+
+interface AppNameProps {
+    className?: string;
+}
+
+export const AppLogo = (props: AppNameProps) => {
+    const { className } = props;
+    return <div className={classNames(styles.text, {}, [className])}>English Simulator</div>;
+};
