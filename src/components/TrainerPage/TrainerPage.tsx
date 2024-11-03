5adad75cc8ade3cae7ec13d20259edee11f82a28
Dmitry Dadychyn <ddadychyn@elinext.com>
2024-11-03 12:18:44 +0300

    feat(f-7): add option selection panel

--- src/components/TrainerPage/TrainerPage.tsx
+++ src/components/TrainerPage/TrainerPage.tsx
@@ -3,10 +3,54 @@ import classNames from 'classnames';
 
 import { Page } from '../Page/Page';
 import { LevelSelectionPanel } from '../LevelSelectionPanel/LevelSelectionPanel';
+import { OptionSelectionPanel } from '../OptionSelectionPanel/OptionSelectionPanel';
 
 import styles from './TrainerPage.module.scss';
 
+export interface Option {
+    sound: boolean;
+    en: boolean;
+    ru: boolean;
+}
+
 const levels = ['A0', 'A1', 'A2', 'B1', 'B2', 'C1'];
+const options: Option[] = [
+    {
+        sound: true,
+        en: false,
+        ru: false,
+    },
+    {
+        sound: false,
+        en: true,
+        ru: false,
+    },
+    {
+        sound: false,
+        en: false,
+        ru: true,
+    },
+    {
+        sound: true,
+        en: false,
+        ru: true,
+    },
+    {
+        sound: true,
+        en: true,
+        ru: false,
+    },
+    {
+        sound: false,
+        en: true,
+        ru: true,
+    },
+    {
+        sound: true,
+        en: true,
+        ru: true,
+    },
+];
 
 interface TrainerPageProps {
     className?: string;
@@ -15,6 +59,7 @@ interface TrainerPageProps {
 export const TrainerPage = (props: TrainerPageProps) => {
     const { className } = props;
     const [selectedLevel, setSelectedLevel] = useState(levels[0]);
+    const [selectedOption, setSelectedOption] = useState(options[0]);
 
     return (
         <Page>
@@ -23,6 +68,12 @@ export const TrainerPage = (props: TrainerPageProps) => {
                     selectedLevel={selectedLevel}
                     onSelectedLevel={setSelectedLevel}
                     levels={levels}
+                />
+                <OptionSelectionPanel
+                    selectedOption={selectedOption}
+                    onSelectedOption={setSelectedOption}
+                    options={options}
+                    className={styles.panel}
                 />
             </div>
         </Page>
