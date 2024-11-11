9d82989280df9e853741fa488414d5bba66a88e1
Dmitry Dadychyn <ddadychyn@elinext.com>
2024-11-11 17:31:33 +0300

    feat(f-18): add right sidebar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import { AppLogo } from '../AppLogo/AppLogo';
+import { RightSidebar } from '../RightSidebar/RightSidebar';
 import styles from './Navbar.module.scss';
 
 export const Navbar = () => {
     return (
         <div className={styles.navbar}>
             <AppLogo className={styles.logoPosition} />
-            <div className={styles.wrapper}>text</div>
+            <RightSidebar />
         </div>
     );
 };
