5f9db4f824350b6aad558a8915b268a730e11809
Dmitry Dadychyn <ddadychyn@elinext.com>
2024-11-14 22:19:50 +0300

    feat(f-22): fix sass

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j9t6gibii1g"
+    "revision": "0.3jgeh9oeeg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
