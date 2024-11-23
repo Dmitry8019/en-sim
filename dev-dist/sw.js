7be29e25914befb2b5e54e1681b99eb1c64d04fe
Dmitry Dadychyn <ddadychyn@elinext.com>
2024-11-23 13:22:11 +0300

    feat(f-31): fix touch handler, reactor code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tu91v0k865o"
+    "revision": "0.4tmusli4nl8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
